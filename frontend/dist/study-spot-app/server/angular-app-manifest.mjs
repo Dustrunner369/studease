a75c989b64b412d3a6cbe29d6b71badba02cc1dd
Matthew <mswift0023@pointloma.edu>
2025-07-19 13:04:52 -0600

    UI changes and styling

--- frontend/dist/study-spot-app/server/angular-app-manifest.mjs
+++ frontend/dist/study-spot-app/server/angular-app-manifest.mjs
@@ -1,14 +1,13 @@
 
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
-  inlineCriticalCss: true,
+  inlineCriticalCss: false,
   baseHref: '/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 1816, hash: '362651ee8da4da5e59a025634e82d51888c096e2d6d71502591d43f4fbae238e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1365, hash: '8932a3be72504f2f31c1240398d83493f48cc8bf28e5ceebc49b9976cb50856a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-NWKEDMF5.css': {size: 19292, hash: 'K99KDEpHFjc', text: () => import('./assets-chunks/styles-NWKEDMF5_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 511, hash: '36fbbc2b921f34f13e0f9d7fd99283e21687eee6a2f8fb82d0f5c212d2ef14d9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1051, hash: 'f90499264c8f483c201ae384771844cbb2999ae3f918bb31e706be4fbf2d56c6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}
   },
 };
